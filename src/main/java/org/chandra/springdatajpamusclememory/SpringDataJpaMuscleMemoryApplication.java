7913d76b0ca6c4b2f66917cecbd313a87216f2ea
Chandra Sekhar Vipparla <code.chandrasekhar@gmail.com>
2025-11-11 09:13:40 +0530

    feat: initialize Spring Data JPA muscle memory project

---
+++ src/main/java/org/chandra/springdatajpamusclememory/SpringDataJpaMuscleMemoryApplication.java
@@ -0,0 +1,13 @@
+package org.chandra.springdatajpamusclememory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaMuscleMemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaMuscleMemoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chandra/springdatajpamusclememory/SpringDataJpaMuscleMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chandra.springdatajpamusclememory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaMuscleMemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
