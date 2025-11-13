16a4d7a22acc402dd39d15ad5df53b880ab5c53b
Chandra Sekhar Vipparla <code.chandrasekhar@gmail.com>
2025-11-13 13:32:24 +0530

    feat: implement Task 4.1.1 – establish bidirectional One-to-One relationship between UserProfile and Address

---
+++ src/main/java/org/chandra/springdatajpamusclememory/topic4_relationships/task1/AddressRepository.java
@@ -0,0 +1,8 @@
+package org.chandra.springdatajpamusclememory.topic4_relationships.task1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address,Long> {
+}
